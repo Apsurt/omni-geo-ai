b541e10cc04071dc82337b3d7007b003f237db82
Apsurt <tymon@becella.eu>
2024-06-13 13:06:10 +0200

    Add docstrings and linter hints

--- omni/coord_generator.py
+++ omni/coord_generator.py
@@ -1,35 +1,69 @@
-from shapely.geometry import Point, Polygon, MultiPolygon
-from resources import all_coords
-from extract_coords import Coordinator
-from typing import List, Generator
-import numpy as np
-import random
+"""Module containing Random Coordinate Generator class."""
 
-class Coordinate_Generator:
-    def __init__(self) -> None:
+from __future__ import annotations
+
+from typing import Generator
+
+import numpy as np
+from extract_coords import Coordinator
+from shapely.geometry import Point
+
+
+class CoordinateGenerator:
+    """Class for generating random coordinates."""
+
+    def __init__(self: CoordinateGenerator) -> None:
+        """Class constructor."""
         parser = Coordinator()
         self.positive_dict, self.negative_dict = parser.get_multipolygon_dicts()
         self.country_list = list(self.positive_dict.keys())
 
-    def is_in_positive(self, country: str, point: Point) -> bool:
+    def is_in_positive(self: CoordinateGenerator, country: str, point: Point) -> bool:
+        """Check if coordinate is in positive polygon.
+
+        :param self: self
+        :type self: CoordinateGenerator
+        :param country: Country name
+        :type country: str
+        :param point: Point to check
+        :type point: Point
+        :return: is point in positive polygon
+        :rtype: bool
+        """
         multipolygon_p = self.positive_dict[country]
-        for polygon in multipolygon_p.geoms:
-            if point.within(polygon):
-                return True
-    
-        return False
-    def is_in_negative(self, country: str, point: Point) -> bool:
+        return any(point.within(polygon) for polygon in multipolygon_p.geoms)
+
+    def is_in_negative(self: CoordinateGenerator, country: str, point: Point) -> bool:
+        """Check if coordinate is in negative polygon.
+
+        :param self: self
+        :type self: CoordinateGenerator
+        :param country: Country name
+        :type country: str
+        :param point: Point to check
+        :type point: Point
+        :return: is point in negative polygon
+        :rtype: bool
+        """
         try:
             multipolygon_n = self.negative_dict[country]
         except KeyError:
             return False
-        for poly_n in multipolygon_n.geoms:
-            if point.within(poly_n):
-                return True
-        return False
-        
-    def get_random_coord_generator(self, country: str) -> Generator[List[Point], None, None]:
+        return any(point.within(poly_n) for poly_n in multipolygon_n.geoms)
 
+    def get_random_coord_generator(
+        self: CoordinateGenerator,
+        country: str,
+        ) -> Generator[list[Point], None, None]:
+        """Create generator yielding list of points.
+
+        :param self: self
+        :type self: CoordinateGenerator
+        :param country: country to make the generator forc
+        :type country: str
+        :rtype: Generator[list[Point], None, None]
+        """
+        max_points = 100
         multipolygon_p = self.positive_dict[country]
 
         areas = [poly.area for poly in multipolygon_p.geoms]
@@ -40,15 +74,13 @@ class Coordinate_Generator:
             poly = np.random.choice(multipolygon_p.geoms, p=weights)
             min_x, min_y, max_x, max_y = poly.bounds
 
-            random_point = Point([random.uniform(min_x, max_x), random.uniform(min_y, max_y)])
-            if self.is_in_positive(country, random_point) and (not self.is_in_negative(country, random_point)):
+            lat = np.random.uniform(min_x, max_x)
+            lon = np.random.uniform(min_y, max_y)
+            random_point = Point([lat, lon])
+            if self.is_in_positive(country, random_point) and \
+               (not self.is_in_negative(country, random_point)):
                 coord_list.append(random_point)
 
-            if len(coord_list) == 100:
+            if len(coord_list) == max_points:
                 yield coord_list
                 coord_list = []
-
-if __name__ == "__main__":
-    cg = Coordinate_Generator()
-    generator = cg.get_random_coord_generator("uk")
-    print(next(generator))
