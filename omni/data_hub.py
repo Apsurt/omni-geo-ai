d70aa69ab564342fe648039206ef906bd51d942e
Apsurt <tymon@becella.eu>
2024-06-13 13:51:10 +0200

    Add docstrings and linter hints

--- omni/data_hub.py
+++ omni/data_hub.py
@@ -1,22 +1,44 @@
-from typing import List, Dict
+"""Module for automated data collection."""
+
+from __future__ import annotations
+
 import os
+from typing import TYPE_CHECKING
+
 import numpy as np
-from PIL import Image
-from shapely.geometry import Point
+from coord_generator import CoordinateGenerator
 from google_api_handler import Handle
-from coord_generator import Coordinate_Generator
+
+if TYPE_CHECKING:
+    from PIL import Image
+    from shapely.geometry import Point
 from resources import Coordinate
 
+
 class DataHub:
-    def __init__(self) -> None:
+    """Automated data collection class."""
+
+    def __init__(self: DataHub) -> None:
+        """DataHub constructor."""
         self.google_api = Handle()
-        generator = Coordinate_Generator()
+        generator = CoordinateGenerator()
 
         self.generator_dict = {}
         for country in generator.country_list:
             self.generator_dict[country] = generator.get_random_coord_generator(country)
 
-    def save_img(self, path: str, country: str, img: Image) -> None:
+    def save_img(self: DataHub, path: str, country: str, img: Image) -> None:
+        """Save image in declared path.
+
+        :param self: self
+        :type self: DataHub
+        :param path: place where country folder can be found
+        :type path: str
+        :param country: country name
+        :type country: str
+        :param img: image to save
+        :type img: Image
+        """
         country = country.lower().replace(" ", "_")
         country_dir = os.path.join(path,country)
         if not os.path.exists(country_dir):
@@ -25,29 +47,95 @@ class DataHub:
         filename = f"{len(files)}.png"
         img.save(country_dir+"/"+filename)
 
-    def point_to_coord(self, point: Point) -> Coordinate:
-        coord = Coordinate(point.x, point.y)
-        return coord
+    def point_to_coord(self: DataHub, point: Point) -> Coordinate:
+        """Convert shapely Point to Coordinate.
 
-    def to_batches(self, v: List, batch_size: int = 100) -> List[List]:
+        :param self: self
+        :type self: DataHub
+        :param point: shapely point
+        :type point: Point
+        :return: converted coordinate
+        :rtype: Coordinate
+        """
+        return Coordinate(point.x, point.y)
+
+    def to_batches(self: DataHub, v: list, batch_size: int = 100) -> list[list]:
+        """Split list into lists of max len of batch_size.
+
+        :param self: self
+        :type self: DataHub
+        :param v: list to split
+        :type v: list
+        :param batch_size: max length of lists in return, defaults to 100
+        :type batch_size: int, optional
+        :return: list of splitted lists
+        :rtype: list[list]
+        """
         return [v[x:x+batch_size] for x in range(0, len(v), batch_size)]
-    
-    def get_stats(self, path: str) -> Dict:
+
+    def get_stats(self: DataHub, path: str) -> dict:
+        """Get dictionary of file count in specified path.
+
+        :param self: self
+        :type self: DataHub
+        :param path: path to get stats from
+        :type path: str
+        :return: dict with dir name and file count
+        :rtype: dict
+        """
         dirs = os.listdir(path)
         stats_dict = {}
         for _dir in dirs:
             files = os.listdir(os.path.join(path, _dir))
             stats_dict[_dir] = len(files)
         return stats_dict
-    
-    def get_total_data_amount(self, path: str):
+
+    def get_total_data_amount(self: DataHub, path: str) -> int:
+        """Sum stats dictionary.
+
+        :param self: self
+        :type self: DataHub
+        :param path: path to get stats from
+        :type path: str
+        :return: sum of files
+        :rtype: int
+        """
         return sum(self.get_stats(path).values())
-    
-    def get_data_standard_deviation(self, path: str):
+
+    def get_data_standard_deviation(self: DataHub, path: str) -> float:
+        """Calculate standard deviation of file counts.
+
+        :param self: self
+        :type self: DataHub
+        :param path: path to get stats from
+        :type path: str
+        :return: standard deviation
+        :rtype: float
+        """
         return np.std(list(self.get_stats(path).values()))
 
-    def get_data(self, max_files: int, path: str, skip: List[str] = []) -> None:
+    def get_data(
+        self: DataHub,
+        max_files: int,
+        path: str,
+        skip: list[str] | None = None,
+        ) -> None:
+        """Mainloop function.
+
+        Collects data till each country has specified amount of files.
+
+        :param self: self
+        :type self: DataHub
+        :param max_files: how much files each country can have
+        :type max_files: int
+        :param path: path to directory
+        :type path: str
+        :param skip: skip certain countries, defaults to None
+        :type skip: list[str] | None, optional
+        """
         print(f"Saving files till each country has {max_files}")
+        if not skip:
+            skip = []
         total = 0
         total_saved = 0
         stats = self.get_stats(path)
